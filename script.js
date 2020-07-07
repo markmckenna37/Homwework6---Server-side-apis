aa81124a28940f5d2e84b7a770adff0279176ab7
Mark <youremail@gmail.comg>
2020-07-06 19:40:29 -0600

    added some html css layouts and pseudocode

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+
+
+
+// new Date() checkout MDN date object
+// openweathermap.org/api (try to fiugre out how to make it in units imperial)
+// localstorage for the cities on the left sidebar
+    // clickable dynamic elements (buttons in this case), this is very similar to activiity 10 unit 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
